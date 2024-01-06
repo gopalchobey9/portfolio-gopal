8ace2eb39d1a3042accc1c49afff72f02a0a3581
gopal chobey <100458189+gopalchobey9@users.noreply.github.com>
2024-01-06 17:04:21 +0530

    skill section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,29 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SKILLS } from '../utils/data'
 import SkillCard from './SkillCard/SkillCard'
+import "./Skills.css"
+import SkillsInfoCard from './SkillsInfoCard/SkillsInfoCard'
 
 const Skills = () => {
-  return (
+    let [selectedSkill, setSelectedSkill] =useState(SKILLS[0]);
 
+    const handlerSelectedSkill =(data)=>{
+      setSelectedSkill(data);
+    }
+
+
+  return (
+    <>  
+    <h5>Technical Proficiency</h5>
     <section className="skills-container">
-        <h5>Technical Proficiency</h5>
+    
         <div className="skills-content">
             <div className='skills'>
-           {SKILLS.map((item) => {
-              return (
-                <SkillCard 
-                key={item.title}
-                iconUrl={item.icon}
-                title={item.title}
-                />
-              )
-          })}
+           
+           {
+           SKILLS.map((item) => {
+             return (
+               <SkillCard 
+               key={item.title}
+               iconUrl={item.icon}
+               title={item.title}
+             isActive={selectedSkill.title === item.title}
+             onClick={()=>{
+              handlerSelectedSkill(item);
+             }}
+               />
+               )
+              })}
             </div>
         <div className="skills-info">
+          <SkillsInfoCard 
+          heading={selectedSkill.title}
+          skills={selectedSkill.skills}
+          />
         </div>
         </div>
 
     </section>
+              </>
   )
 }
 
--- src/components/utils/data.js
+++ src/components/utils/data.js
@@ -4,7 +4,7 @@ export const SKILLS =[
         icon:"/images/frontend.png",
         skills:[
             {skill:"HTML5",percentage:"80%"},
-            {skill:"CSS3",percentage:"75%%"},
+            {skill:"CSS3",percentage:"75%"},
             {skill:"JavaScript",percentage:"70%"},
             {skill:"React.Js",percentage:"75%"},
 
@@ -15,7 +15,7 @@ export const SKILLS =[
         icon:"/images/backend.png",
         skills:[
             {skill:"Node.Js",percentage:"80%"},
-            {skill:"Express.js",percentage:"75%%"},
+            {skill:"Express.js",percentage:"75%"},
         ],
     },
     {
@@ -30,7 +30,7 @@ export const SKILLS =[
         icon:"/images/tool.png",
         skills:[
             {skill:"Git & Github",percentage:"85%"},
-            {skill:"Visula Studio Code",percentage:"75%%"},
+            {skill:"Visula Studio Code",percentage:"75%"},
             {skill:"Webpack",percentage:"70%"},
             {skill:"Responsive Design",percentage:"65%"},
 
